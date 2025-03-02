70a49d0a3b5933295e99e2a2fb767a1532702026
Russell Gordon <rgordon@lcs.on.ca>
2025-03-02 15:21:09 -0500

    Initial project organization; added groups for MVVM design pattern.

---
+++ JokeFinderLesson/Views/JokeView.swift
@@ -0,0 +1,23 @@
+//
+//  JokeView.swift
+//  JokeFinderLesson
+//
+//  Created by Russell Gordon on 2025-03-02.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    
+    // MARK: Stored properties
+    
+    
+    // MARK: Computed properties
+    var body: some View {
+        Text("Jokes will show up here... soon!")
+    }
+}
+
+#Preview {
+    JokeView()
+}
