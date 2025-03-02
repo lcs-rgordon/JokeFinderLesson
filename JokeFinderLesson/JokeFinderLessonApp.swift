2e1bd6bb28c9d5981f1d4af7073e19c691bd184e
Russell Gordon <rgordon@lcs.on.ca>
2025-03-02 15:19:05 -0500

    Initial Commit

---
+++ JokeFinderLesson/JokeFinderLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderLessonApp.swift
+//  JokeFinderLesson
+//
+//  Created by Russell Gordon on 2025-03-02.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
