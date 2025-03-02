872eae8f51b3502e4f5e962fc8374f04cb4f06f6
Russell Gordon <rgordon@lcs.on.ca>
2025-03-02 15:37:21 -0500

    Added an initial model to match the format of the expected JSON response.

---
+++ JokeFinderLesson/Model/Joke.swift
@@ -0,0 +1,26 @@
+//
+//  Joke.swift
+//  JokeFinder
+//
+//  Created by Russell Gordon on 2025-03-01.
+//
+
+import Foundation
+
+struct Joke: Identifiable, Codable {
+    
+    // MARK: Stored properties
+    let type: String
+    let setup: String?
+    let punchline: String?
+    let id: Int
+        
+}
+
+// Create an example joke for testing purposes
+let exampleJoke = Joke(
+    type: "general",
+    setup: "Why couldn't the kid go to see the pirate movie?",
+    punchline: "Because it was rated arrrrr!",
+    id: 310
+)
