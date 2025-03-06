7e14a5ab78c0dc33e925924c56418b3eb8e002cc
Russell Gordon <rgordon@lcs.on.ca>
2025-03-05 19:19:51 -0500

    Added app icon, and made it possible to share a saved joke.

--- JokeFinderLesson/Views/FavouriteJokesView.swift
+++ JokeFinderLesson/Views/FavouriteJokesView.swift
@@ -39,6 +39,16 @@ struct FavouriteJokesView: View {
                                 }
                             }
                             
+                            // Share
+                            ShareLink(
+                                "Share",
+                                item: currentJoke.setupAndPunchline,
+                                preview: SharePreview(
+                                    "Share Joke",
+                                    image: Image("ShareJokeImage")
+                                )
+                            )
+                            
                         }
                     }
                     .listStyle(.plain)
